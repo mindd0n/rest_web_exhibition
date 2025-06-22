03e1efa5daea98f1a96eedf1cd69decab3b7aa5a
mindd0n <sittingmin@naver.com>
2025-06-22 12:57:12 +0900

    feat: Adjust Out button size and position

--- gallery/src/components/RightBottomControls.jsx
+++ gallery/src/components/RightBottomControls.jsx
@@ -56,7 +56,7 @@ const RightBottomControls = () => {
         window.open('/exit-page/index.html', '_blank');
     };
 
-    const buttonStyle = {
+    const normalButtonStyle = {
         width: '80px',
         height: '80px',
         border: 'none',
@@ -65,10 +65,14 @@ const RightBottomControls = () => {
         marginBottom: '20px',
         transition: 'transform 0.2s ease',
     };
-
-    const buttonHoverStyle = {
-        ...buttonStyle,
-        transform: 'scale(1.1)',
+    
+    const outButtonStyle = {
+        ...normalButtonStyle,
+        width: '250px',
+        height: '250px',
+        marginBottom: '0px',
+        position: 'relative',
+        top: '100px',
     };
 
     const containerStyle = {
@@ -108,7 +112,7 @@ const RightBottomControls = () => {
             <div style={containerStyle}>
                 {/* 3. Out 버튼 */}
                 <button
-                    style={buttonStyle}
+                    style={outButtonStyle}
                     onMouseEnter={(e) => {
                         e.target.style.transform = 'scale(1.1)';
                         const img = e.target.querySelector('img') || e.target;
@@ -128,13 +132,13 @@ const RightBottomControls = () => {
                     <img
                         src="/images/buttons/icon_out.png"
                         alt="Exit"
-                        style={{ width: '100%', height: '100%', objectFit: 'contain', padding: '2px' }}
+                        style={{ width: '100%', height: '100%', objectFit: 'contain', padding: '0px' }}
                     />
                 </button>
 
                 {/* 2. Map 버튼 */}
                 <button
-                    style={buttonStyle}
+                    style={normalButtonStyle}
                     onMouseEnter={(e) => e.target.style.transform = 'scale(1.1)'}
                     onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
                     onClick={toggleMap}
@@ -148,7 +152,7 @@ const RightBottomControls = () => {
 
                 {/* 1. Music 버튼 (토글) */}
                 <button
-                    style={buttonStyle}
+                    style={normalButtonStyle}
                     onMouseEnter={(e) => e.target.style.transform = 'scale(1.1)'}
                     onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
                     onClick={toggleMusic}
